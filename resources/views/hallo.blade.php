25d01ec1d9cbe594b57b0dd3725d559f646b0406
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-04-07 13:05:05 +0700

    Fix Navbar

--- resources/views/hallo.blade.php
+++ resources/views/hallo.blade.php
@@ -16,60 +16,28 @@
 </head>
 
 <body class="">
+<div class="flex flex-col w-full">
     <div class="drawer">
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content flex flex-col">
             <!-- Navbar -->
-
-            {{-- <div class="w-full fixed navbar shadow z-50 bg-base-300"> --}}
-            <div class="navbar bg-base-100">
-                <div class="flex-1">
-                    <a class="btn btn-ghost normal-case text-xl">Logo</a>
-                </div>
-                <div class="flex-none">
-                    <ul class="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabindex="0">
-                            <a>
-                                Parent
-                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20"
-                                    height="20" viewBox="0 0 24 24">
-                                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                                </svg>
-                            </a>
-                            <ul class="p-2 bg-base-100">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                        <li><a>Item 3</a></li>
-                        <li><a>Item 3</a></li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-            </div>
-            {{-- <div class="flex-none lg:hidden">
+            <div class="w-full navbar z-50 bg-base-300">
+                <div class="flex-none lg:hidden">
                     <label for="my-drawer-3" class="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            class="inline-block w-6 h-6 stroke-current">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16"></path>
-                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </label>
                 </div>
-                <div class="flex-1 px-2 mx-2">Logo</div>
+                <div class="flex-1 px-2 mx-2">Navbar Title</div>
                 <div class="flex-none hidden lg:block">
                     <ul class="menu menu-horizontal">
                         <!-- Navbar menu content here -->
                         <li><a>Navbar Item 1</a></li>
                         <li><a>Navbar Item 2</a></li>
                     </ul>
-                </div> --}}
-            {{-- </div> --}}
+                </div>
+            </div>
             <!-- Page content here -->
-            <div class="hero min-h-screen bg-base-200 bg-white dark:bg-slate-800">
+            <div class="hero min-h-screen bg-base-200 bg-white dark:bg-slate-800 ">
                 <div class="carousel w-full">
                     <div id="slide1" class="carousel-item relative w-full">
                         <img src="https://picsum.photos/seed/picsum/200/300" class="w-full h-500" />
@@ -124,6 +92,8 @@
 
         </div>
     </div>
+</div>
+
 
 </body>
 
